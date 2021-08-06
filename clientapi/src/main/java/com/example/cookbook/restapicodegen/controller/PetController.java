af806b87bf03f6c15004871ad7080728d78f5820
Renzo Racioppi <rracioppi@imolainformatica.it>
2021-08-06 13:23:34 +0200

    adattamenti

--- clientapi/src/main/java/com/example/cookbook/restapicodegen/controller/PetController.java
+++ clientapi/src/main/java/com/example/cookbook/restapicodegen/controller/PetController.java
@@ -1,8 +1,8 @@
 package com.example.cookbook.restapicodegen.controller;
 
-import org.openapitools.client.api.PetApiClient;
-import org.openapitools.server.api.PetApi;
-import org.openapitools.server.model.Pet;
+import com.example.cookbook.restapicodegen.client.api.PetApiClient;
+import com.example.cookbook.restapicodegen.server.api.PetApi;
+import com.example.cookbook.restapicodegen.server.model.Pet;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,9 +10,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class PetController implements PetApi {
 
+    @Autowired
+    PetApiClient petApiClient;
+
     @Override
     public ResponseEntity<Pet> getPetById(Long petId) {
-        //ResponseEntity<org.openapitools.client.model.Pet> asd = petApiClient.getPetById(petId);
-        return ResponseEntity.ok(new Pet().id(petId).name("dog"));
+        ResponseEntity<com.example.cookbook.restapicodegen.client.model.Pet> response = petApiClient.getPetById(petId);
+        return ResponseEntity.ok(new Pet().id(response.getBody().getId()).name(response.getBody().getName()));
     }
 }
